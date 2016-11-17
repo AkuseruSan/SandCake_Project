4a4730d46e3b510e8b547dcfc2996fc43d935a86
AkuseruSan <axelcastells.gamedev@gmail.com>
2016-11-17 14:22:19 +0100

    Added 2 Worlds Renderer

---
+++ Assets/Code/Core/InputManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager {
+
+    private InputManager()
+    {
+
+    }
+
+    public static InputManager Instance()
+    {
+        InputManager im = new InputManager();
+        return im;
+    }
+}
