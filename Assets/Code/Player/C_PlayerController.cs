5d7ab65d746bee64a5e1cd336c80160954ac5ce1
Unknown <molivertauler@gmail.com>
2016-11-17 19:42:36 +0100

    first physics commit

--- Assets/Code/Player/C_PlayerController.cs
+++ Assets/Code/Player/C_PlayerController.cs
@@ -1,18 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent(typeof(C_PlayerBehaviour))]
-[RequireComponent(typeof(Rigid))]
+[RequireComponent(typeof(Rigidbody2D))]
 
 public class C_PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    #region variables
+    private Vector2 contactNormal;
+    private Rigidbody2D rBody;
+
+    public float speed;
+    public Vector2 jump;
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+        contactNormal = Vector2.zero;
+        rBody = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        Movement();
+        RotatePlayer();
 	}
+
+    #region events
+
+    void OnCollisionStay2D(Collision2D other)
+    {
+        foreach(ContactPoint2D contact in other.contacts)
+        {
+            contactNormal += contact.normal;
+        }
+        contactNormal = contactNormal.normalized;
+    }
+
+    #endregion
+
+    #region functions
+
+    void RotatePlayer()
+    {
+        
+    }
+
+    void Movement()
+    {
+        rBody.velocity = new Vector2(contactNormal.y * speed, rBody.velocity.y);
+    }
+
+    void Jump()
+    {
+        rBody.AddForce(jump, ForceMode2D.Impulse);
+    }
+
+    #endregion
 }
