8815833426a55c73333535c4b194eb7de9867ac4
Unknown <akuseruakai@gmail.com>
2016-11-17 16:33:38 +0100

    Update

---
+++ Assets/Code/Player/C_PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(C_PlayerBehaviour))]
+[RequireComponent(typeof(Rigid))]
+
+public class C_PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
