abf6e4af41bd6f241a68edcb6000c3003345beea
AkuseruSan <axelcastells.gamedev@gmail.com>
2016-11-17 02:22:12 +0100

    Init

---
+++ Assets/Code/Game/CS_RenderBlend.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class CS_RenderBlend : MonoBehaviour
+{
+    private Camera sourceCam;
+    private Texture2D sourceRender, destinationRender;
+
+    public Camera depthCam, destCam;
+    public RenderTexture renderTexture;
+
+    void Start()
+    {
+        depthCam.depthTextureMode = DepthTextureMode.Depth;
+    }
+
+    void OnPreRender()
+    {
+        destCam.SetTargetBuffers(renderTexture.colorBuffer, renderTexture.depthBuffer);
+        //Resources.Load("RenderTex");
+    }
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Code/Shaders/CameraBlendShader.shader
@@ -0,0 +1,19 @@
+﻿Shader "CameraBlend/Mask" {
+
+	SubShader{
+		// Render the mask after regular geometry, but before masked geometry and
+		// transparent things.
+
+		Tags{ "Queue" = "Geometry+100" }
+
+		// Don't draw in the RGBA channels; just the depth buffer
+
+		ColorMask 0
+		ZTest Less
+		ZWrite On
+
+		// Do nothing specific in the pass:
+
+		Pass{}
+	}
+}
