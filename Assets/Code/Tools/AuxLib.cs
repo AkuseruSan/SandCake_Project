0272f3cbfa8b1c6730472bea22217cb5804d6537
Unknown <akuseruakai@gmail.com>
2016-11-18 19:25:52 +0100

    Parallax System Started

---
+++ Assets/Code/Tools/AuxLib.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AuxLib {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+[System.Serializable]
+public struct DualSprite
+{
+    public Sprite day;
+    public Sprite night;
+    public int sortOrder;
+}
---
+++ Assets/Code/Tools/ParallaxController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ParallaxController : MonoBehaviour {
+
+    public List<DualSprite> parallaxSprites;
+	// Use this for initialization
+	void Start () {
+        CreateParallaxLayers();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void CreateParallaxLayers()
+    {
+        for(int i = 0; i < parallaxSprites.Count; i++)
+        {
+            GameObject go = new GameObject("ParallaxLayer " + i);
+            go.transform.parent = this.transform;
+            go.AddComponent<ParallaxLayerBehaviour>();
+
+            GameObject goDay = new GameObject("DayLayer");
+            goDay.AddComponent<SpriteRenderer>().sprite = parallaxSprites[i].day;
+            goDay.transform.parent = go.transform;
+
+            GameObject goNight = new GameObject("NightLayer");
+            goNight.AddComponent<SpriteRenderer>().sprite = parallaxSprites[i].night;
+            goNight.GetComponent<SpriteRenderer>().sortingOrder = parallaxSprites[i].sortOrder;
+            goNight.transform.parent = go.transform;
+        }
+    }
+}
