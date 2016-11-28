ade32acba51a24f4e0fdbc7beb5563bcd0dbebbe
AkuseruSan <axelcastells.gamedev@gmail.com>
2016-11-28 20:08:38 +0100

    Update

--- Assets/Code/UI/MainMenuAnimator.cs
+++ Assets/Code/UI/MainMenuAnimator.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class MainMenuAnimator : MonoBehaviour {
 
+    private Animator myAnim;
 	// Use this for initialization
 	void Start () {
-	
+        myAnim = GetComponent<Animator>();
+
 	}
 	
 	// Update is called once per frame
