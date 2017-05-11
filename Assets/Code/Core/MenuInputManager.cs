d978656c0ba8bbc6091bf69747d9a2bfdd0cfed9
AkuseruSan <akuseruakai@gmail.com>
2017-05-11 13:27:29 +0200

    Update

--- Assets/Code/Core/MenuInputManager.cs
+++ Assets/Code/Core/MenuInputManager.cs
@@ -13,4 +13,29 @@ public class MenuInputManager : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    private RaycastHit2D GetHit()
+    {
+
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y, 0)), Vector3.forward, Mathf.Infinity);
+
+        if (hit)
+        {
+            return hit;
+        }
+
+
+        return new RaycastHit2D();
+    }
+
+    private void FixedUpdate()
+    {
+        if(Input.touchCount > 0)
+        {
+            //if(GetHit().transform.GetComponent<Button>() != null)
+            //{
+
+            //}
+        }
+    }
 }
