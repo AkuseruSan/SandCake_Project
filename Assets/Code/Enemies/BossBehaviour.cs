394c6570fac0a8d019197e9fa456a69a96106d22
AkuseruSan <akuseruakai@gmail.com>
2017-05-25 12:54:26 +0200

    Update

---
+++ Assets/Code/Enemies/BossBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBehaviour : MonoBehaviour {
+
+    private enum State { IDDLE, LOAD, ATTACK, DIE }
+    private State state;
+
+    public GameObject lance;
+
+    private Queue<GameObject> lances;
+
+    private GameObject player;
+    private PlayerController playerController;
+	// Use this for initialization
+	void Start () {
+        player = GameCore.Instance.player;
+        playerController = GameCore.Instance.playerController;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void LoadLances()
+    {
+
+    }
+}
---
+++ Assets/Code/Enemies/LanceBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanceBehaviour : MonoBehaviour {
+
+    private enum State { IDDLE, SHOOT }
+    private State state;
+    // Use this for initialization
+    void Start() {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        switch (state)
+        {
+            case State.IDDLE:
+                {
+
+                }
+                break;
+            case State.SHOOT:
+                {
+
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+
+}
