16fa22e416f02b777e5fb1bb96ee727526881c2c
AkuseruSan <akuseruakai@gmail.com>
2017-05-25 13:28:27 +0200

    Update

--- Assets/Code/Enemies/BossBehaviour.cs
+++ Assets/Code/Enemies/BossBehaviour.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BossBehaviour : MonoBehaviour {
 
-    private enum State { IDDLE, LOAD, ATTACK, DIE }
+    private enum State { IDDLE, LOAD, AIM, ATTACK, DIE }
     private State state;
 
     public GameObject lance;
@@ -13,19 +13,77 @@ public class BossBehaviour : MonoBehaviour {
 
     private GameObject player;
     private PlayerController playerController;
+
+    private float distanceToPlayer;
+    private static int MAX_LANCES;
+    private int lancesCounter;
 	// Use this for initialization
 	void Start () {
+        distanceToPlayer = 10;
+
         player = GameCore.Instance.player;
         playerController = GameCore.Instance.playerController;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        transform.position = new Vector3(player.transform.position.x + distanceToPlayer, player.transform.position.y, player.transform.position.z);
+
+        switch (state)
+        {
+            case State.IDDLE:
+                {
+                    state = State.LOAD;
+                }
+                break;
+            case State.LOAD:
+                {
+                    LoadLances();
+                    state = State.AIM;
+                }
+                break;
+            case State.AIM:
+                {
+                    
+                }
+                break;
+            case State.ATTACK:
+                {
+
+                }
+                break;
+            case State.DIE:
+                {
+
+                }
+                break;
+            default:
+                break;
+        }
+    }
 
     void LoadLances()
     {
+        lancesCounter = MAX_LANCES;
 
+        StartCoroutine(LoadLance());
+    }
+
+    private IEnumerator LoadLance()
+    {
+        GameObject g = Instantiate(lance, transform.position, Quaternion.identity, this.transform);
+
+        yield return new WaitForSeconds(1);
+
+        lancesCounter--;
+        if(lancesCounter <= 0)
+        {
+            
+        }
+        else
+        {
+            StartCoroutine(LoadLance());
+        }
     }
 }
